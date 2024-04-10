d583d086f271c60f8e215a5678b2e587164c1c79
Ariana Nagumo <arinagumo@gmail.com>
2024-04-10 12:18:42 -0600

    Initial Commit

---
+++ Nasa/NasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaApp.swift
+//  Nasa
+//
+//  Created by Ariana Santillán on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
